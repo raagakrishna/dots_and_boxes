74a1c8dc015fb4ecf7580db2b8fa9474a03a83f1
george_pauer <george.pauer1@vcontractor.co.za>
2023-06-21 19:19:38 +0200

    DB Connecting successfully Specify env vars : DB_HOST, DB_NAME, DB_USER, DB_PASS

---
+++ server/src/main/java/za/dots/db/DatasourceConnection.java
@@ -0,0 +1,21 @@
+package za.dots.db;
+
+import java.sql.*;
+
+public class DatasourceConnection {
+
+    Connection connection;
+
+    private String cleanInputValue(String string) {
+        return string.replaceAll("[^a-zA-Z\\d]", string);
+    }
+
+    public DatasourceConnection() throws SQLException {
+        this.connection = DriverManager.getConnection("jdbc:sqlserver://"
+                + System.getenv("DB_HOST")//127.0.0.1:1433 for local
+                + ";encrypt=true;trustServerCertificate=true;database="
+                + System.getenv("DB_NAME") + ";",
+                System.getenv("DB_USER"),
+                System.getenv("DB_PASS"));
+    }
+}
