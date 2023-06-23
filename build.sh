ebd2774b1689174ae7977e2042780fec6245808c
george_pauer <george.pauer1@vcontractor.co.za>
2023-06-23 20:54:26 +0200

    Auto deploy script

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ sudo fuser -k 7070/tcp
 sudo fuser -k 7071/tcp
 sudo mvn clean package --file ./server/pom.xml
 sudo mvn clean package --file ./resource_server/pom.xml
-sudo nohup java -jar ./server/dots_boxes-1.0-SNAPSHOT-jar-with-dependencies.jar &
-sudo nohup java -jar ./resource_server/dots_boxes-1.0-SNAPSHOT-jar-with-dependencies.jar &
+sudo nohup java -jar ./server/target/dots_boxes-1.0-SNAPSHOT-jar-with-dependencies.jar &
+sudo nohup java -jar ./resource_server/target/dots_boxes-1.0-SNAPSHOT-jar-with-dependencies.jar &
 sudo rm -rf /var/www/mysite/
 sudo echo "SITE REMOVED"
 sudo cp -r ./site/ /var/www/mysite/
