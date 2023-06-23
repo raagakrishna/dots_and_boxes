cdd8c3cded3562a56aacccd52c1bc55fe57f63bb
Raaga Krishna <raaga@bbd.co.za>
2023-06-23 19:34:15 +0200

    Initial page created and made responsive.

---
+++ site/js/common.js
@@ -0,0 +1,41 @@
+function addHeader() {
+    var myHeader = document.getElementById("header");
+    myHeader.classList.add("purple_box");
+
+    // adding game name
+    var h1 = document.createElement("h1");
+    h1.textContent = "Dots and Boxes";
+    myHeader.appendChild(h1);
+
+    // adding dropdown 
+    var dropdown = document.createElement("section");
+    dropdown.classList.add("dropdown");
+
+    // adding username
+    var username = document.createElement("a");
+    username.setAttribute("id", "username");
+    username.innerHTML = "Raaga Krishna  ";
+    dropdown.appendChild(username);
+
+    // adding dropdown content 
+    var ul = document.createElement("ul");
+    ul.classList.add("dropdown-content");
+
+    // adding li
+    var li1 = document.createElement("li");
+    li1.textContent = "Your played games";
+    ul.appendChild(li1);
+
+    var li2 = document.createElement("li");
+    li2.textContent = "Logout";
+    ul.appendChild(li2);
+
+    dropdown.appendChild(ul);
+    myHeader.appendChild(dropdown);
+}
+
+function addFooter() {
+    var myFooter = document.getElementById("footer");
+    myFooter.classList.add("purple_box");
+    myFooter.textContent = "\u00A9 2023 Dots and Boxes";
+}
