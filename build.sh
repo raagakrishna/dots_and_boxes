5ef8c027cf50605a76db616a1ea25aab685a8408
george_pauer <george.pauer1@vcontractor.co.za>
2023-06-23 16:25:00 +0200

    adding sudo to script commands (potential permission issues)

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #Remote build script for push
-cd /home/ubuntu/dots_and_boxes
-git pull
-fuser -k 7070/tcp
-fuser -k 7071/tcp
-mvn clean package --file ./server/pom.xml
-mvn clean package --file ./resource_server/pom.xml
-nohup java -jar ./server/dots_boxes-1.0-SNAPSHOT-jar-with-dependencies.jar &
-nohup java -jar ./resource_server/dots_boxes-1.0-SNAPSHOT-jar-with-dependencies.jar &
-rm -rf /var/www/mysite/
-echo "SITE REMOVED"
-cp -r ./site/ /var/www/mysite/
-systemctl reload nginx
-echo "NEW SITE LOADED"
+sudo cd /home/ubuntu/dots_and_boxes
+sudo git pull
+sudo fuser -k 7070/tcp
+sudo fuser -k 7071/tcp
+sudo mvn clean package --file ./server/pom.xml
+sudo mvn clean package --file ./resource_server/pom.xml
+sudo nohup java -jar ./server/dots_boxes-1.0-SNAPSHOT-jar-with-dependencies.jar &
+sudo nohup java -jar ./resource_server/dots_boxes-1.0-SNAPSHOT-jar-with-dependencies.jar &
+sudo rm -rf /var/www/mysite/
+sudo echo "SITE REMOVED"
+sudo cp -r ./site/ /var/www/mysite/
+sudo systemctl reload nginx
+sudo echo "NEW SITE LOADED"
