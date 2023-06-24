58225a9b3292dbe64debc41ce5b7309aec66a2a0
Daniel Trevor Harding <danielha@bbd.co.za>
2023-06-24 09:05:22 +0200

    Lowering java level and changing record back to regular class

--- identity_server/src/main/java/za/resources/models/User.java
+++ identity_server/src/main/java/za/resources/models/User.java
@@ -1,4 +1,14 @@
 package za.resources.models;
 
-public record User(String userName) {
+public class User {
+
+    private final String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
 }
