7fac75fb21269e2b0df1171e21df7cc8f69ca678
Raaga Krishna <raaga@bbd.co.za>
2023-06-24 11:48:12 +0200

    Added all the responses for player, players and room.

--- server/src/main/java/za/dots/controllers/PlayersCrudHandler.java
+++ server/src/main/java/za/dots/controllers/PlayersCrudHandler.java
@@ -1,5 +1,6 @@
 package za.dots.controllers;
 
+import io.javalin.http.NotFoundResponse;
 import za.dots.controllers.interfaces.PlayersApi;
 import za.dots.models.Player;
 
@@ -9,6 +10,11 @@ public class PlayersCrudHandler implements PlayersApi {
 
     @Override
     public List<Player> getPlayers() {
-        return null;
+        List<Player> players = null; // TODO: fetch players from database
+
+        if (players == null) {
+            throw new NotFoundResponse("No players found.");
+        }
+        return players;
     }
 }
