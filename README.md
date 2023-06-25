90b905cdb924feab28c5a7845a23b8f8a1f8f2a3
Daniel Trevor Harding <danielha@bbd.co.za>
2023-06-25 15:16:13 +0200

    Public and private keys should now work

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # dots_and_boxes
+## Identity Server
+### Database
+Create a sql server database and run the info.sql script to create the necessary tables
+### Environment Variables
+Set the following environment variables for the server to work
+- RSA private key location: privateKey
+- RSA public key location: publicKey
+- Issuer for jwt token: issuer
+- Audience for jwt token: audience
+- The username for db access: dbUsername
+- The password for db access: dbPassword
+- The url for db access: dbUrl
+- The schema which you are going to connect: dbName
+    - If you run the script as is the dbName = OAuth
+### RSAKeys
+Run the following commands to generate the RSA keys for the program
+- openssl genrsa -out privatekey.pem 2048
+- openssl rsa -in privatekey.pem -out publickey.pem -pubout
+
--- identity_server/src/main/java/za/resources/cryptography/RSAKeys.java
+++ identity_server/src/main/java/za/resources/cryptography/RSAKeys.java
@@ -9,11 +9,12 @@ import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 public class RSAKeys {
 
     public static RSAPrivateKey getRSAPrivateKey(String fileLoc) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        final byte[] bytes = getFileBytes(fileLoc);
+        final byte[] bytes = getFileBytes(fileLoc, "PRIVATE");
         PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(bytes);
         KeyFactory kf = KeyFactory.getInstance("RSA");
         return (RSAPrivateKey) kf
@@ -21,17 +22,23 @@ public class RSAKeys {
     }
 
     public static RSAPublicKey getRSAPublicKey(String fileLoc) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        final byte[] bytes = getFileBytes(fileLoc);
+        final byte[] bytes = getFileBytes(fileLoc, "PUBLIC");
         X509EncodedKeySpec keySpec = new X509EncodedKeySpec (bytes);
         KeyFactory kf = KeyFactory.getInstance("RSA");
         return (RSAPublicKey) kf
                 .generatePublic(keySpec);
     }
 
-    private static byte[] getFileBytes(String fileLoc) {
+    private static byte[] getFileBytes(String fileLoc, String key) {
         byte[] fileBytes;
+        System.out.println(fileLoc);
         try (FileInputStream fis = new FileInputStream(fileLoc)) {
-            fileBytes = fis.readAllBytes();
+            fileBytes = Base64.getDecoder().decode(new String(fis.readAllBytes())
+                    .replaceAll("\\n", "")
+                    .replaceAll("-----BEGIN " + key + " KEY-----", "")
+                    .replaceAll("-----END " + key + " KEY-----", "")
+                    .trim()
+                    .getBytes());
         } catch (IOException e) {
             fileBytes = new byte[0];
         }
