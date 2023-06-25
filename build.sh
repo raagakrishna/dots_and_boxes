f2b29937294297454b1d01265e886719be5b9ae7
george_pauer <george.pauer1@vcontractor.co.za>
2023-06-25 21:32:49 +0200

    update build script for remote server

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #Remote build script for push
 git pull
-fuser -k 80/tcp
-fuser -k 443/tcp
-fuser -k 7071/tcp
+lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
+lsof -t -i tcp:443 -s tcp:listen | sudo xargs kill
 mvn clean package --file ./server/pom.xml
 mvn clean package --file ./identity_server/pom.xml
-nohup java -jar ./server/target/dots_boxes-1.0-SNAPSHOT-jar-with-dependencies.jar &
-nohup java -jar ./identity_server/target/dots_boxes-1.0-SNAPSHOT-jar-with-dependencies.jar &
+env DB_HOST="" \
+DB_NAME="" \
+DB_USER="" \
+DB_PASS="" \
+CERT_PATH="" \
+CERT_PATH_KEY="" \
+CERT_PASS="" \
+java -jar ./server/target/dots_boxes-1.0-SNAPSHOT-jar-with-dependencies.jar
+#nohup java -jar ./identity_server/target/dots_boxes-1.0-SNAPSHOT-jar-with-dependencies.jar &
